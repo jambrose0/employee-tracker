c5a1eff54df33a1b9565f2f1db2a5d4e78c10ce3
jambrose0 <jambrose0518@gmail.com>
2022-05-27 09:54:17 -0400

    refacted and fixed merge conflicts

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
-<<<<<<< HEAD
-var inquirer = require("inquirer");
-const { Action } = require("rxjs/internal/scheduler/Action");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "5GcXHEHKPR5?gs*y",
-  database: "staffing",
-});
-
-const gatherInfo = (data) => {
-  return inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "Add Employee",
-          "Update Employee Role",
-          "View All Roles",
-          "Add Role",
-          "View All Departments",
-          "Add Department",
-          "Quit",
-        ],
-      },
-    ])
-    .then((answers) => {
-      if (answers.action === "View All Employees") {
-        console.log("Heres the department:");
-        const sql = `SELECT * FROM departments`;
-        db.query(sql, (err, rows) => {
-          if (err) {
-            console.log(error.message);
-          }
-          //new commands
-          console.table(rows);
-=======
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+
 
 const manageEmployees = () => {
   inquirer
@@ -94,13 +55,7 @@ const manageEmployees = () => {
         addDepartment().then(() => {
           console.table("Employee Edited!");
           manageEmployees;
->>>>>>> feature/server
         });
       }
     });
 };
-<<<<<<< HEAD
-
-module.exports();
-=======
->>>>>>> feature/server
